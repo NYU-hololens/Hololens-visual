964707942e06f246c5ca84961ac1c916f71d4672
quintoben <584621005@qq.com>
2017-08-01 17:10:20 -0400

    Add static Class SelectedColumnInfo to store selected column information
     Class SelectedColumnInfo
    func: addColumnName(string tableName,string columnName)----add the column name to the column name set of specific table
    func: getColumnNameSet(string tableName)----return the column name set for specific table
    func: removeColumnName(string tableName,string columnName)----remove the column name in specific columnNameSet

--- Assets/Scripts/ColumnManager.cs
+++ Assets/Scripts/ColumnManager.cs
@@ -12,23 +12,48 @@ public class ColumnManager : MonoBehaviour, IInputClickHandler
     // Use this for initialization  
     public void OnInputClicked(InputClickedEventData eventData)
     {
+        //get the parent object !! right now just hardcode 
+        GameObject parent = GameObject.FindGameObjectWithTag("SphereTable1");
+
+        string tableName = parent.tag;
+        string columnName = this.gameObject.transform.Find("ColumnNameText").GetComponent<TextMesh>().text;
+        
+        
+
+        //dehighlight the object
         if (highlight==true)
         {
             for (int i = 0; i < defaultMaterials.Length; i++)
             {
                 defaultMaterials[i].SetFloat("_Highlight", .0f);
             }
-            //existingDetail.SetActive(false);
             highlight = false;
+            //remove the column name to the SelectedColumnInfo
+            SelectedColumnInfo.removeColumnName(tableName, columnName);
+
+            Debug.Log(tableName + " " + columnName + " remove " + SelectedColumnInfo.getColumnNameSet(tableName).Count);
+            Debug.Log("Column name in set");
+            foreach(string cn in SelectedColumnInfo.getColumnNameSet(tableName))
+            {
+                Debug.Log(cn);
+            }
             return;
         }
 
-        
+        //highlight the column
         for (int i = 0; i < defaultMaterials.Length; i++)
         {
             defaultMaterials[i].SetFloat("_Highlight", .5f);
         }
         highlight = true;
+        //add the column name to the SelectedColumnInfo
+        SelectedColumnInfo.addColumnName(tableName, columnName);
+        Debug.Log(tableName + " " + columnName + " add " + SelectedColumnInfo.getColumnNameSet(tableName).Count);
+        Debug.Log("Column name in set");
+        foreach (string cn in SelectedColumnInfo.getColumnNameSet(tableName))
+        {
+            Debug.Log(cn);
+        }
 
     }
 
