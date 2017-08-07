caf501c81797c98addf86250ffb6022ca743ab4c
chenghsi.chao <curiojustus@gmail.com>
2017-08-07 11:03:57 -0400

    Add sqlitedb

---
+++ Assets/Scripts/dbscripts/ExistingDBScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ExistingDBScript : MonoBehaviour {
+
+	public Text DebugText;
+
+	// Use this for initialization
+	void Start () {
+		var ds = new DataService ("mainsql.db");
+		var rows = ds.GetRows ();
+        // ToConsole(people);
+        rows = ds.GetPersonsNamedRoberto ();
+        ToConsole(rows);
+	}
+	
+	private void ToConsole(IEnumerable<FL_insurance_sample_test1> rows){
+		foreach (var row in rows) {
+			// ToConsole(person.ToString());
+            Debug.Log(row.ToString());
+        }
+	}
+
+	private void ToConsole(string msg){
+		DebugText.text += System.Environment.NewLine + msg;
+		Debug.Log (msg);
+	}
+
+}
---
+++ Assets/Scripts/dbscripts/FL_insurance_sample_test1.cs
@@ -0,0 +1,34 @@
+﻿using SQLite4Unity3d;
+using System;
+
+public class FL_insurance_sample_test1
+{
+
+	[PrimaryKey, AutoIncrement]
+    public int policyID { get; set; }
+
+    public string statecode { get; set; }
+    public string county { get; set; }
+    public float eq_site_limit { get; set; }
+    public float hu_site_limit { get; set; }
+    public float fl_site_limit { get; set; }
+    public float fr_site_limit { get; set; }
+    public float tiv_2011 { get; set; }
+    public float tiv_2012 { get; set; }
+    public float eq_site_deductible { get; set; }
+    public float hu_site_deductible { get; set; }
+    public float fl_site_deductible { get; set; }
+    public float fr_site_deductible { get; set; }
+    public float point_latitude { get; set; }
+    public float point_longitude { get; set; }
+    public string line { get; set; }
+    public string construction { get; set; }
+    public int point_granularity { get; set; }
+    public DateTime Created_At { get; set; }
+    public DateTime Valid_Until { get; set; }
+
+	public override string ToString ()
+	{
+		return string.Format ("[Row: policyID={0}, statecode={1},  county={2}, point_granularity={3}]", policyID, statecode, county, point_granularity);
+	}
+}
